19991f967514275e5b5742ef6e25571c850bfaf4
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-04-01 00:45:08 +0400

    Update vars.py

--- roft/vars.py
+++ roft/vars.py
@@ -16,7 +16,7 @@ class RoftConfiguration:
     ApiID = os.environ.get('API_ID', 15954332)
     ApiHASH = os.environ.get('API_HASH', "85adea6f1eaf068b707703b4846a9ced") 
 
-    TelegramBot = os.environ.get('TELEGRAM', "5869735539:AAFJIEdqvHHptb875nstYUVxETh9WRpqC9E")
+    TelegramBot = os.environ.get('TELEGRAM', "5869735539:AAFbsTd162Ec5KAxokZC87tU4_4SkEPGDlw")
     SpecialUsers = int(os.environ.get('CREATOR_ID', "5134595693"))
 
     MongoDatabaseURI = os.environ.get('DATABASE_URI', "mongodb+srv://srnpxkb007:srnpxkb007@cluster0.i2tsrxy.mongodb.net/?retryWrites=true&w=majority")
